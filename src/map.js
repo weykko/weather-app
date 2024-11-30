ebf711056b1b670197d4cdfbeed01959d6119363
aleksandr <a.d.beznutrov@urfu.me>
2024-11-30 08:23:49 +0500

    deploy and create readme

--- src/map.js
+++ src/map.js
@@ -3,9 +3,9 @@ export function generateMap(lat, lon) {
     mapContainer.classList.add('map-container');
 
     const mapIframe = document.createElement('iframe');
-    mapIframe.src = `https://www.openstreetmap.org/export/embed.html?bbox=${lon - 0.05}%2C${lat - 0.05}%2C${lon + 0.05}%2C${lat + 0.05}&layer=mapnik&marker=${lat}%2C${lon}`;
+    //mapIframe.src = `https://www.openstreetmap.org/export/embed.html?bbox=${lon - 0.05}%2C${lat - 0.05}%2C${lon + 0.05}%2C${lat + 0.05}&layer=mapnik&marker=${lat}%2C${lon}`;
     //static map
-    //mapIframe.src = `https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=408&height=408&center=lonlat:${lon},${lat}&zoom=10&marker=lonlat:${lon},${lat};color:%23007bff;size:medium&apiKey=cd3386bf280348a7931c870606015114`
+    mapIframe.src = `https://maps.geoapify.com/v1/staticmap?style=osm-carto&width=408&height=408&center=lonlat:${lon},${lat}&zoom=10&marker=lonlat:${lon},${lat};color:%23007bff;size:medium&apiKey=cd3386bf280348a7931c870606015114`
     mapIframe.allowFullscreen = true;
     mapIframe.style.border = 0;
 
